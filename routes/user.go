de622c423cb4bc580ba994bb91c7d5691be1ea36
vronnn <zhafrandzaky@gmail.com>
2023-10-15 23:05:29 +0700

    fix: alter patch into put user update

--- routes/user.go
+++ routes/user.go
@@ -14,7 +14,7 @@ func User(route *gin.Engine, userController controller.UserController, jwtServic
 		routes.POST("", userController.Register)
 		routes.POST("/login", userController.Login)
 		routes.DELETE("/", middleware.Authenticate(jwtService), userController.Delete)
-		routes.PATCH("/", middleware.Authenticate(jwtService), userController.Update)
+		routes.PUT("", middleware.Authenticate(jwtService), userController.Update)
 		routes.GET("/me", middleware.Authenticate(jwtService), userController.Me)
 		routes.POST("/verify-email", userController.VerifyEmail)
 		routes.POST("/verification-email", userController.SendVerificationEmail)
